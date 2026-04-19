583cd83c55ae57dd9880e535d2890a6f3eae7cac
Never Mind <38748801+secfb@users.noreply.github.com>
2026-04-19 18:05:53 +0300

    Update print.go

--- internal/afmt/print.go
+++ internal/afmt/print.go
@@ -31,7 +31,7 @@ const Banner = `        .+++:.            :                             .+++.
 
 const (
 	// Version is used to display the current version of Amass.
-	Version = "v5.1.1"
+	Version = "v5.1.2-Output-Fix"
 
 	// Author is used to display the Amass Project Team.
 	Author = "OWASP Amass Project - @owaspamass"
